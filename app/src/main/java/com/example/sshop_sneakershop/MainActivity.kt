ceedad125d308a7035de8a9d625f4d94e624e96b
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2022-03-23 14:20:21 +0700

    layout: complete change password and app settings screen

--- app/src/main/java/com/example/sshop_sneakershop/MainActivity.kt
+++ app/src/main/java/com/example/sshop_sneakershop/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.sshop_sneakershop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_app_settings)
+
+        var topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+
+        topAppBar.setNavigationOnClickListener {
+            // Handle navigation icon press
+            Toast.makeText(this, "Navigation Icon Pressed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
