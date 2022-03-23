d8faa726b6626ac762983264256e7c287d46d4f8
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2022-03-23 12:33:28 +0700

    layout: complete register screen

--- app/src/main/java/com/example/sshop_sneakershop/MainActivity.kt
+++ app/src/main/java/com/example/sshop_sneakershop/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.activity_sign_up)
     }
 }
