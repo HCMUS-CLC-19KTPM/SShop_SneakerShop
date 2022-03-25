eb8bb7c3f7548aa0fea0005de790830799746299
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2022-03-25 23:52:17 +0700

    add user screen

--- app/src/main/java/com/example/sshop_sneakershop/MainActivity.kt
+++ app/src/main/java/com/example/sshop_sneakershop/MainActivity.kt
@@ -12,6 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.activity_user)
     }
 }
---
+++ app/src/main/java/com/example/sshop_sneakershop/user/UserActivity.kt
@@ -0,0 +1,27 @@
+package com.example.sshop_sneakershop.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.gridlayout.widget.GridLayout
+import com.example.sshop_sneakershop.R
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+
+        val infoView = findViewById<GridLayout>(R.id.gridLayout)
+        infoView.setVisibility(View.GONE)
+
+        val showInfoBtn = findViewById<Button>(R.id.showInfoBtn)
+        showInfoBtn.setOnClickListener {
+            if(infoView.getVisibility() == View.GONE) {
+                infoView.setVisibility(View.VISIBLE)
+            } else {
+                infoView.setVisibility(View.GONE)
+            }
+        }
+    }
+}
