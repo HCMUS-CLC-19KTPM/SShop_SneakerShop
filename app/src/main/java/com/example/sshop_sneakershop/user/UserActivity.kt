e1aa4bc47ad17d39930f9ab6e16f5a3ce332c9a0
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2022-03-28 11:41:31 +0700

    edit user screen

--- app/src/main/java/com/example/sshop_sneakershop/user/UserActivity.kt
+++ app/src/main/java/com/example/sshop_sneakershop/user/UserActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sshop_sneakershop.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,16 +13,26 @@ class UserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
 
+        val editInfoBtn = findViewById<Button>(R.id.profile_button_editInfo)
+        editInfoBtn.setVisibility(View.GONE)
+
         val infoView = findViewById<GridLayout>(R.id.gridLayout)
         infoView.setVisibility(View.GONE)
 
-        val showInfoBtn = findViewById<Button>(R.id.showInfoBtn)
+        val showInfoBtn = findViewById<Button>(R.id.profile_button_showAccDetail)
         showInfoBtn.setOnClickListener {
             if(infoView.getVisibility() == View.GONE) {
                 infoView.setVisibility(View.VISIBLE)
+                editInfoBtn.setVisibility(View.VISIBLE)
             } else {
                 infoView.setVisibility(View.GONE)
+                editInfoBtn.setVisibility(View.GONE)
             }
+        }
+
+        editInfoBtn.setOnClickListener {
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
