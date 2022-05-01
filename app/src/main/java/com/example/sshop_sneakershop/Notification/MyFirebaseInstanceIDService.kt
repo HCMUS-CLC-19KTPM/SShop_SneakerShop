210606c05df9afff31b59eef7b8b1f053a84dcba
kygor <namworkmc@gmail.com>
2022-05-01 16:17:12 +0700

    Added push notification from FCM feature.

--- app/src/main/java/com/example/sshop_sneakershop/Notification/MyFirebaseInstanceIDService.kt
+++ app/src/main/java/com/example/sshop_sneakershop/Notification/MyFirebaseInstanceIDService.kt
@@ -1,19 +1,90 @@
 package com.example.sshop_sneakershop.Notification
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.RingtoneManager
+import android.net.Uri
 import android.util.Log
-import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import com.example.sshop_sneakershop.Homepage.Home
+import com.example.sshop_sneakershop.Product.views.ProductDetail
+import com.example.sshop_sneakershop.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseInstanceIDService : FirebaseMessagingService() {
 
+    private val TAG = "FireBaseMessagingService"
+    var NOTIFICATION_CHANNEL_ID = "net.larntech.notification"
+    val NOTIFICATION_ID = 100
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        Log.e("FCM", "From: " + remoteMessage.notification?.title)
+
+        val title = remoteMessage.notification?.title
+        val body = remoteMessage.notification?.body
+        var itemId: String? = null
+        if (remoteMessage.data.isNotEmpty()) {
+            itemId = remoteMessage.data["item-id"].toString()
+        }
+        showNotification(applicationContext, title, body, itemId)
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.e("FCM", "Refreshed token: $token")
+    }
+
+    private fun showNotification(
+        context: Context,
+        title: String?,
+        message: String?,
+        itemId: String?
+    ) {
+        val intent = Intent(context, ProductDetail::class.java)
+        intent.putExtra("item-id", itemId)
+        intent.data = Uri.parse("custom://" + System.currentTimeMillis())
+        intent.action = "actionstring" + System.currentTimeMillis()
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+        val pi =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val notification: Notification =
+            NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.logo_size_invert)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.logo_size_invert
+                    )
+                )
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pi)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .setWhen(System.currentTimeMillis())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentTitle(title).build()
+
+        val notificationManager = context.getSystemService(
+            Context.NOTIFICATION_SERVICE
+        ) as NotificationManager
+
+        val notificationChannel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            title,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+
+        notificationManager.createNotificationChannel(notificationChannel)
+        notificationManager.notify(NOTIFICATION_ID, notification)
     }
 }
